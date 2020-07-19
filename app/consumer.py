302125f768f4e2438a44e13c3d4b5932efb9ba59
Stepan Kozurak <step.kozbvb@gmail.com>
2020-07-19 17:10:13 +0200

    Modified consumer

--- app/consumer.py
+++ app/consumer.py
@@ -1,5 +1,3 @@
-import os
-import json
 from json import JSONDecodeError
 from marshmallow import Schema, fields
 import nsq
@@ -11,41 +9,37 @@ class MessageSchema(Schema):
     destination = fields.Str()
 
 
-TCP_ADDRESSES = [os.getenv('TCP_ADDRESSES', 'http://127.0.0.1:4150')]
-
-USERS = {}
 ORIGIN = {}
 DESTINATION = {}
+USERS = {}
 
 
-def occurance(value, value_type):
+def occurrence(value, value_type):
     if value in value_type.keys():
         value_type[value] += 1
+        return value_type[value]
     value_type[value] = 1
+    return 1
 
 
 def handler(message):
     schema = MessageSchema()
     try:
         result = schema.loads(message.body.decode())
-        occurance(result['user'], USERS)
-        occurance(result['origin'], ORIGIN)
-        occurance(result['destination'], DESTINATION)
-        print("Users:", USERS)
-        print("Origin:", ORIGIN)
-        print("Destination:", DESTINATION)
+        occurrence(result['user'], USERS)
+        occurrence(result['destination'], DESTINATION)
+        occurrence(result['origin'], ORIGIN)
+        print(USERS)
+        print(DESTINATION)
+        print(ORIGIN)
+
         return True
     except JSONDecodeError:
         return False
 
 
-r = nsq.Reader(
-    message_handler=handler,
-    nsqd_tcp_addresses=TCP_ADDRESSES,
-    topic='nsq_example',
-    channel='consumer_channel',
-    lookupd_poll_interval=15
-)
+r = nsq.Reader(message_handler=handler, nsqd_tcp_addresses='127.0.0.1:4150',
+               topic='nsq_example', channel='chanel', lookupd_poll_interval=15)
 
 if __name__ == '__main__':
     nsq.run()
